e98d74020dad851c1c83ce5d6df02485e2f5fe28
Anuj Nirmal <anujrajeshnirmal@gmail.com>
2022-06-19 21:08:26 +0530

    Added coming soon message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Homepage from './pages/Homepage';
 import './App.css';
+
 
 function App() {
   return (
     <div>
-      
+      <Homepage />
     </div>
   );
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header';
+
+const Homepage = () => {
+  return (
+    <div style={{ fontSize: "40px", textAlign: "center"}}>
+        <h1>Coming Soon!</h1>
+    </div>
+  )
+}
+
+export default Homepage
